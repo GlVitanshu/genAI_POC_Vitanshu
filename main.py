a5849dcfc895d2c6aaee2f090f2c979e76f876bc
GlVitanshu <166478197+GlVitanshu@users.noreply.github.com>
2024-09-16 14:48:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from fabonacci import FibonacciClass
-f = FibonacciClass()
-f.Fibonacci(10)
+from find_area_of_circle import *
+if __name__ == "__main__":
+    f = FibonacciClass()
+    f.Fibonacci(10)
