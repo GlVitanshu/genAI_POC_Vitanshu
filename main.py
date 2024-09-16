8d1527f236b86d8d449d3f18f560845b5f0af563
GlVitanshu <166478197+GlVitanshu@users.noreply.github.com>
2024-09-16 14:50:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fabonacci import FibonacciClass
 from find_area_of_circle import *
-if __name__ == "__main__":
+from prime_number import *
+
+def main():
     f = FibonacciClass()
     f.Fibonacci(10)
+
+
+if __name__ == "__main__":
+    main()
