b32f0e54997088e6ce6341540e416cfb890e59e5
GlVitanshu <166478197+GlVitanshu@users.noreply.github.com>
2024-09-16 14:14:56 +0530

    Update fabonacci.py
    Add fabonacci class.

--- fabonacci.py
+++ fabonacci.py
@@ -1,6 +1,7 @@
 # Function for nth Fibonacci number 
 
-def Fibonacci(n): 
+class FibonacciClass:
+     def Fibonacci(n): 
 	if n<0: 
 		print("Incorrect input") 
 	# First Fibonacci number is 0 
@@ -11,7 +12,3 @@ def Fibonacci(n):
 		return 1
 	else: 
 		return Fibonacci(n-1)+Fibonacci(n-2) 
-
-# Driver Program 
-
-print(Fibonacci(9)) 
