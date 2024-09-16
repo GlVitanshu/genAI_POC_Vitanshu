968be344627dccbdbe7a07d7874489032bafd53d
GlVitanshu <166478197+GlVitanshu@users.noreply.github.com>
2024-09-16 14:20:40 +0530

    Update fabonacci.py

--- fabonacci.py
+++ fabonacci.py
@@ -1,14 +1,14 @@
 # Function for nth Fibonacci number 
 
 class FibonacciClass:
-     def Fibonacci(n): 
-	if n<0: 
-		print("Incorrect input") 
-	# First Fibonacci number is 0 
-	elif n==1: 
-		return 0
-	# Second Fibonacci number is 1 
-	elif n==2: 
-		return 1
-	else: 
-		return Fibonacci(n-1)+Fibonacci(n-2) 
+    def Fibonacci(self, n): 
+    	if n<0: 
+    		print("Incorrect input") 
+    	# First Fibonacci number is 0 
+    	elif n==1: 
+    		return 0
+    	# Second Fibonacci number is 1 
+    	elif n==2: 
+    		return 1
+    	else: 
+    		return self.Fibonacci(n-1)+self.Fibonacci(n-2) 
